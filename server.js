bc216b66d6d41946390d2070dd0fd79da4709d0f
Hameme21 <hamimasir5@gmail.com>
2026-07-05 00:50:35 +0600

    Update server.js

--- server.js
+++ server.js
@@ -208,7 +208,7 @@ const server = http.createServer((request, response) => {
         return;
     }
 
-    const pathname = url.pathname === '/' ? '/admin.html' : url.pathname;
+    const pathname = url.pathname === '/' ? '/index.html' : url.pathname;
     const requestedPath = path.normalize(path.join(rootDir, decodeURIComponent(pathname)));
     const relativePath = path.relative(rootDir, requestedPath);
 
